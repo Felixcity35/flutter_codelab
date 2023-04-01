aeb7c7a2c1cdb316094844bbc857fd2470833963
felixcity <Felixarchibong@gmail.com>
2023-04-01 19:55:02 +0100

    first commit auth

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:complete/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+// TODO(codelab user): Get API key
+//const clientId = 'YOUR_CLIENT_ID';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
